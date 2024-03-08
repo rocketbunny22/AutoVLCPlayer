d529a8ff0a78aba39615d1885d0a495f11d9f756
rocketbunny22 <65988765+rocketbunny22@users.noreply.github.com>
2024-03-07 19:58:21 -0500

    Add files via upload

---
+++ AutoVLCpurple.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Path to the VLC executable
+VLC_PATH="/usr/bin/vlc"
+
+# Path to the playlist file
+PLAYLIST_FILE="/home/mthrift/MTpurple.m3u"
+
+# Start time in HH:MM format (24-hour clock)
+START_TIME="08:30"
+
+# End time in HH:MM format (24-hour clock)
+END_TIME="19:00"
+
+
+    # Check if the current time is between start and end times
+    CURRENT_TIME=$(date +%H:%M)
+    if [[ "$CURRENT_TIME" > "$START_TIME" && "$CURRENT_TIME" < "$END_TIME" ]]; then
+        # Launch VLC with the playlist
+        "$VLC_PATH" -f --loop "$PLAYLIST_FILE" &
+        # Store the PID of VLC process
+        VLC_PID=$!
+        # Wait for VLC to finish
+        wait $VLC_PID
+    else
+        echo "Current time is not within the specified range."
+   fi
+fi
+
